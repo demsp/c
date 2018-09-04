ebec0df4f5d3d0350b8adcdc6962e4e5d4d5100e
Dima <dserp18@gmail.com>
2018-09-04 16:28:05 +0300

    Update copy1.c

--- linux_api/copy1.c
+++ linux_api/copy1.c
@@ -4,8 +4,7 @@
 #ifndef BUF_SIZE /* Allow "cc -D" to override definition */
 #define BUF_SIZE 1024
 #endif
-int
-main(int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 int inputFd, outputFd, openFlags;
 mode_t filePerms;
