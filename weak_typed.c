6bfa9d4c1d81f07c00469650cceaa17a7dc6540a
demsp <dserp18@gmail.com>
2019-05-07 12:50:17 +0300

    Create weak_typed.c

---
+++ weak_typed.c
@@ -0,0 +1,13 @@
+// https://habr.com/ru/post/308484/
+
+#include <stdio.h>
+int main(int argc, char **argv){
+	int x;
+	printf("Hello\n");
+	printf("\n");
+	printf("%d",x);
+	printf("\n");
+	int a1= "a" + 1;
+	printf("%d",a1);	
+	return 0;
+}
