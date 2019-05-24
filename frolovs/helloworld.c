7f46da60014f6f037f08abcc0a5bf4bdae370604
demsp <dserp18@gmail.com>
2019-05-24 09:58:45 +0300

    Create helloworld.c

---
+++ frolovs/helloworld.c
@@ -0,0 +1,4 @@
+ Cannot Create D:\tc\Bin\tcdef.dpr
+ Cannot Create D:\tc\Bin\tcdef.dsk
+ Cannot Create D:\tc\Bin\tcconfig.tc
+ clear the "read only" attribute of these files and it will goes on fine. 
